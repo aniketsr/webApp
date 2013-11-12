10c3a32a6f6a3b49d59c365e8c3fd36c61550eaa
ANU <aniket@sabkuch.com>
2013-11-12 13:14:20 +0530

    add2 complete!!!

--- first.js
+++ first.js
@@ -1,8 +1,23 @@
 var http = require('http');
-var _return = 'Bing Bong';
+var url = require('url');
 
-  http.createServer(function (req, res) {
-        res.writeHead(200, {'Content-Type': 'text/plain'});
+var operation = {
+	add2: function(a,b){ return a + b},
+	accumulate: function(c){ return c}
+}
 
-    res.end('Hiiiiiiiiiiiiii!!!!');
+http.createServer(function (req, res) {
+	var pathname = url.parse(req.url).pathname;
+	var requrl =  url.parse(req.url,true);
+	var first = requrl.query.first;
+	var second = requrl.query.second;
+	// var input = pathname.split('/');
+	// var op = operation[input[1]], 
+	// num1 = parseInt(input[2]), num2 = parseInt(input[3]);
+	var result = (+first)+(+second);
+
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end(result.toString(),'utf-8');
+
 }).listen(8282,"localhost");
+  console.log("\nRunning on localhost 8282!!!");
